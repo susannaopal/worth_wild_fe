c773102587399a37b0223ad97c027e93e1309345
Susanna Carey <susannaopal@gmail.com>
2022-03-26 17:06:09 -0400

    Create login form page

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,34 @@
+<script>
+ export default {
+   data() {
+     return {
+       username: "",
+       password: ""
+     }
+   }
+ }
+</script>
+
+<template>
+  <form>
+    <label>Username:</label>
+    <input 
+      type="text"
+      name="username"
+      required
+      v-model="username"
+    />
+    <label>Password:</label>
+    <input 
+      type="text"
+      name="password"
+      required
+      v-model="password"
+    />
+  </form>
+</template>
+
+
+<style>
+
+</style>
