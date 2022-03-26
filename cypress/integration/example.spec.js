de6353c5c665b91d0089e052074fe7eed50d6452
Kai Kuller <kavakai@gmail.com>
2022-03-25 18:02:49 -0600

    Change visit in sample test

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -2,7 +2,7 @@
 
 describe("My First Test", () => {
   it("visits the app root url", () => {
-    cy.visit("http://localhost:5050/");
+    cy.visit("http://localhost:3000/");
     cy.contains("h1", "You did it!");
   });
 });
