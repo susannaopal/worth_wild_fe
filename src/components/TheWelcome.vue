c782a100078a6a1e4a609df82ea8768e8b255cf0
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-03-27 11:44:05 -0600

    Remove unused folders and files from fe repo.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,69 +1,78 @@
 <script setup>
-  import { RouterLink } from "vue-router";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <body>
-    <img required src="../assets/worth_wild.png" class="logo"/>
+    <img required src="../assets/worth_wild.png" class="logo" />
     <div class="landing-page-links">
       <RouterLink to="/home" class="landing-btns">Guest</RouterLink>
       <RouterLink to="/login" class="landing-btns">Log In</RouterLink>
     </div>
-      <p class="mission-statement">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet est placerat in. Pulvinar etiam non quam lacus suspendisse. Mauris vitae ultricies leo integer. Tincidunt arcu non sodales neque sodales ut. Eget est lorem ipsum dolor. Eu consequat ac felis donec et odio pellentesque diam. Enim ut tellus elementum sagittis vitae et leo. Ut pharetra sit amet aliquam id diam. Tortor posuere ac ut consequat semper viverra nam. Semper viverra nam libero justo. Viverra maecenas accumsan lacus vel facilisis. Mauris ultrices eros in cursus turpis massa. Dui accumsan sit amet nulla.
-      </p>
-    </body>
+    <p class="mission-statement">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Sit amet est placerat
+      in. Pulvinar etiam non quam lacus suspendisse. Mauris vitae ultricies leo
+      integer. Tincidunt arcu non sodales neque sodales ut. Eget est lorem ipsum
+      dolor. Eu consequat ac felis donec et odio pellentesque diam. Enim ut
+      tellus elementum sagittis vitae et leo. Ut pharetra sit amet aliquam id
+      diam. Tortor posuere ac ut consequat semper viverra nam. Semper viverra
+      nam libero justo. Viverra maecenas accumsan lacus vel facilisis. Mauris
+      ultrices eros in cursus turpis massa. Dui accumsan sit amet nulla.
+    </p>
+  </body>
 </template>
 
 <style>
-  body {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
-  .logo {
-    border: 3px solid red;
-    height: 200px;
-    padding: -100px;
-  }
+.logo {
+  border: 3px solid red;
+  height: 200px;
+  padding: -100px;
+}
 
-  .mission-statement {
-    color: #432A0B;
-    background-color: #E9E7DD;
-    width: 50%;
-    padding: 35px 25px;
-    border-radius: 20px; 
-  }
+.mission-statement {
+  color: #432a0b;
+  background-color: #e9e7dd;
+  width: 50%;
+  padding: 35px 25px;
+  border-radius: 20px;
+}
 
-  a {
-    text-decoration: none;
-  }
+a {
+  text-decoration: none;
+}
 
-  .landing-page-links{
-    display: flex;
-    justify-content: space-around;
-    width: 20vw;
-  }
+.landing-page-links {
+  display: flex;
+  justify-content: space-around;
+  width: 20vw;
+}
 
-  .landing-btns {
-    background-color: #556D1D;
-    color: #E9E7DD;
-    box-shadow: 0px 5px #334111;
-    border-radius: 50px;
-    height: 5vh;
-    width: 10vw;
-    margin: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-  }
+.landing-btns {
+  background-color: #556d1d;
+  color: #e9e7dd;
+  box-shadow: 0px 5px #334111;
+  border-radius: 50px;
+  height: 5vh;
+  width: 10vw;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
 
-  .landing-btns:hover {
-    transition: .2s;
-    transform: scale(1.1);
-    background-color: #E9E7DD;
-    color: #556D1D;
-  }
+.landing-btns:hover {
+  transition: 0.2s;
+  transform: scale(1.1);
+  background-color: #e9e7dd;
+  color: #556d1d;
+}
 </style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,85 +1,75 @@
 <script>
- export default {
-   data() {
-     return {
-       username: "",
-       password: ""
-     }
-   }
- }
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+};
 </script>
 
 <template>
   <form>
     <div class="input-div">
       <label>Username:</label>
-      <input 
-        type="text"
-        name="username"
-        required
-        v-model="username"
-      />
+      <input type="text" name="username" required v-model="username" />
     </div>
     <div class="input-div">
       <label>Password:</label>
-      <input 
-        type="text"
-        name="password"
-        required
-        v-model="password"
-      />
+      <input type="text" name="password" required v-model="password" />
     </div>
     <button class="login-btn" type="submit">Login</button>
   </form>
 </template>
 
 <style>
-  form {
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    background-color: #3B4B13;
-    padding: 50px;
-    height: 40vh;
-    width: 30vw;
-    border-radius: 25px;
-  }
+form {
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #3b4b13;
+  padding: 50px;
+  height: 40vh;
+  width: 30vw;
+  border-radius: 25px;
+}
 
-  label, input {
-    color: #E9E7DD;
-  }
+label,
+input {
+  color: #e9e7dd;
+}
 
-  input {
-    border-radius: 50px;
-    height: 25px;
-    width: 20em;
-    margin-top: 5px;
-    border: 3px solid #C8C097;
-  }
+input {
+  border-radius: 50px;
+  height: 25px;
+  width: 20em;
+  margin-top: 5px;
+  border: 3px solid #c8c097;
+}
 
-  .input-div {
-    display: flex;
-    flex-direction: column;
-  }
+.input-div {
+  display: flex;
+  flex-direction: column;
+}
 
-  .login-btn {
-    color: #432A0B;
-    background-color: #E9E7DD;
-    width: 20%;
-    border-radius: 25px;
-    height: 35px;
-    border: 3px solid #BCB8A1;
-    font-size: large;
-    cursor: pointer;
-  }
+.login-btn {
+  color: #432a0b;
+  background-color: #e9e7dd;
+  width: 20%;
+  border-radius: 25px;
+  height: 35px;
+  border: 3px solid #bcb8a1;
+  font-size: large;
+  cursor: pointer;
+}
 
-  .login-btn:hover {
-    transition: .2s;
-    transform: scale(1.2);
-    background-color: #556D1D;
-    color: #E9E7DD;
-  }
+.login-btn:hover {
+  transition: 0.2s;
+  transform: scale(1.2);
+  background-color: #556d1d;
+  color: #e9e7dd;
+}
 </style>
-
