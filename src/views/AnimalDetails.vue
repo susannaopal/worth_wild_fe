931f361f0f45401dae2d6d7b8ef4d5e4d5e4d766
Brian Peterson <bpeterson1579@gmail.com>
2022-03-28 15:56:18 -0600

    Make animal details more dynamic

--- src/views/AnimalDetails.vue
+++ src/views/AnimalDetails.vue
@@ -6,7 +6,7 @@
     data() {
       return {
         id : 1,
-        type: "animal",
+        type: 'animal',
         attributes: {
           common_name: 'american black bear',
           scientific_name: 'Ursus americanus',
@@ -18,18 +18,19 @@
           genus: 'Ursus',
           biome: 'Terrestrial',
           region: {
-            'states/territory': ['Alaska', 'Oregon', 'Washington', 'Manitoba'],
-            },
+            statesTerritories: ['Alaska', 'Oregon', 'Washington', 'Manitoba'],
+          },
           vulnerability: {
-            'gRank': 'g5',
-            'gRankReasons': 'Secure'
-            },
+            gRank: 'g1',
+            gRankReasons: 'Critically Imperiled'
+          },
           habitats: ['Woodland - Conifer', 'Woodland - Hardwood'],
           habitatComments: 'Black bears inhabit forests and nearby openings, including forested wetlands.',
-          'threatImpactComments': 'Locally threatened by habitat loss and interference by humans.',
-          'shortTermTrend': 'Populations have increased recently in the northeastern U.S.',
+          threatImpactComments: 'Locally threatened by habitat loss and interference by humans.',
+          shortTermTrend: 'Populations have increased recently in the northeastern U.S.',
           popSize: '10,000 to >1,000,000 individuals',
-          imageUrl: 'https://i.natgeofe.com/n/079d885f-8e97-4e79-a6f1-d7578892399b/4403088.jpg'
+          imageUrl: 'https://i.natgeofe.com/n/079d885f-8e97-4e79-a6f1-d7578892399b/4403088.jpg',
+          hostUrl: 'host.example.com'
         }
       }
     },
@@ -38,40 +39,68 @@
       
     }
   }
-
 </script>
-
 
 <template>
   <body>
     <NavBar />
     <section class="animal-details">
-      <div class="animal-picture">
-        <h2>{{ this.attributes.common_name.toUpperCase() }}</h2>
-        <h3>{{ this.attributes.vulnerability.gRankReasons }}</h3>
-        <img require src=this.attributes.imageUrl alt="animal pic" />
+      <div :style="{'background-image': `url(${this.attributes.imageUrl})`}" class="animal-picture">
+        <h2 class="name">{{ this.attributes.common_name.toUpperCase() }}</h2>
+        <h3 class="status">{{ this.attributes.vulnerability.gRankReasons }}</h3>
       </div>
       <section class="animal-facts">
         <div class="scientific-class-div">
-          <p class="scientific-classification">Phylum:</p>
-          <p class="scientific-classification">Class:</p>
-          <p class="scientific-classification">Order:</p>
-          <p class="scientific-classification">Family:</p>
-          <p class="scientific-classification">Genus:</p>
-          <p class="scientific-classification">Species:</p>
+          <p class="scientific-classification">Phylum: {{ this.attributes.phylum }}</p>
+          <p class="scientific-classification">Class: {{ this.attributes.taxclass }}</p>
+          <p class="scientific-classification">Order: {{ this.attributes.taxorder }}</p>
+          <p class="scientific-classification">Family: {{ this.attributes.family }}</p>
+          <p class="scientific-classification">Genus: {{ this.attributes.genus }}</p>
+          <p class="scientific-classification">Species: {{ this.attributes.scientific_name }}</p>
         </div>
         <div class="threats-div">
-          <p class="threats-info">There are only 2,700 to 4,000 Bengal tigers left in the world. Over the last 100 years, Bengal tigers have lost 93% of their former habitat. </p>
-          <p class="threats-info">Threats to the Bengal Tiger
-          The clearance of forests, an increasing human population and a greater demand for tiger products have all contributed to the tiger's decline. Most tigers are now protected and living in reserves, so the destruction of their habitat is not so great a danger, but as tigers become rarer, the value of a dead tiger has increased. </p>
+          <p class="threats-info">Currently there are {{ this.attributes.popSize }} in America. {{ this.attributes.shortTermTrend }}</p>
+          <h4>Threats to the {{ this.attributes.common_name.toUpperCase() }}:</h4>
+          <p class="threats-info">{{ this.attributes.habitatComments }} {{ this.attributes.threatImpactComments }}</p>
         </div>
       </section>
     </section>
+    <div class="bottom-btns">
+      <button class="landing-btns">Favorite</button>
+      <RouterLink to="/guest" class="landing-btns">Back</RouterLink>
+    </div>
   </body>
 </template>
 
 <style>
   .animal-details {
-    width: 95vw;
+    background-color: #e9e7dd;
+    border-radius: 50px;
+    padding: 10px;
+    width: 60vw;
+    margin-top: 20px;
+    z-index: -5;
+  }
+
+  .animal-picture {
+    height: 30%;
+  }
+  
+  .animal-pic {
+    width: 500px;
+    /* z-index: -2; */
+    position: absolute;
+  }
+
+  .animal-facts {
+    padding-top: 600px;
+  }
+
+  .name {
+    z-index: 1;
+  }
+
+  .bottom-btns {
+    display: flex;
   }
 </style>
