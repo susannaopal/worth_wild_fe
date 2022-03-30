93dd5413842b2fc09a0564f6cea37a88f83c837e
Susanna Carey <susannaopal@gmail.com>
2022-03-30 19:48:00 -0400

    Write tests for login page

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -16,9 +16,30 @@ describe('Landing Page User Flow', () => {
   it('should be able to visit the url', () => {
     cy.url()
       .should('eq', 'http://localhost:5050/');
-  })
+  });
 
   it('should see a logo', () => {
+    cy.get('body')
+      .find('img')
+      .should('be.visible')
+      });
 
-  })
+      it('should see a button for a guest',() => {
+        cy.get('body')
+          .contains('Guest')
+          .should('be.visible')
+      });
+
+      it('should see a button for a user to login',() => {
+        cy.get('body')
+          .contains('Log In')
+          .should('be.visible')
+      });
+
+    it('should see a mission statement', () => {
+       cy.get('body')
+          .get('p')
+          .contains('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet est placerat in. Pulvinar etiam non quam lacus suspendisse. Mauris vitae ultricies leo integer. Tincidunt arcu non sodales neque sodales ut. Eget est lorem ipsum dolor. Eu consequat ac felis donec et odio pellentesque diam. Enim ut tellus elementum sagittis vitae et leo. Ut pharetra sit amet aliquam id diam. Tortor posuere ac ut consequat semper viverra nam. Semper viverra nam libero justo. Viverra maecenas accumsan lacus vel facilisis. Mauris ultrices eros in cursus turpis massa. Dui accumsan sit amet nulla.')
+          .should('be.visible')
+    });
 })
