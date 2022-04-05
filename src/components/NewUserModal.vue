0b5bab2d263c2d367334c8ba091ff4961f83fc51
Kai Kuller <kavakai@gmail.com>
2022-04-05 11:56:40 -0600

    Address tab issue

--- src/components/NewUserModal.vue
+++ src/components/NewUserModal.vue
@@ -15,16 +15,16 @@
 </script>
 
 <template>
-<transition name="modal">
-  <form v-if="show">
-    <input type="text" placeholder="First Name" v-model="firstName"/>
-    <input type="text" placeholder="Last Name" v-model="lastName" />
-    <input type="text" placeholder="Email" v-model="email" />
-    <input type="text" placeholder="Username" v-model="username" />
-    <input type="password" placeholder="Password"/>
-    <button type="submit">Submit</button>
-  </form>
-  </transition>
+  <transition name="modal">
+    <form v-if="show">
+      <input type="text" placeholder="First Name" v-model="firstName"/>
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <input type="text" placeholder="Email" v-model="email" />
+      <input type="text" placeholder="Username" v-model="username" />
+      <input type="password" placeholder="Password"/>
+      <button type="submit">Submit</button>
+    </form>
+    </transition>
 </template>
 
 
