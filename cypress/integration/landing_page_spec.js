2836311baa84874385441cc665a12cb097195897
Susanna Carey <susannaopal@gmail.com>
2022-04-05 19:51:18 -0400

    Update mission statement for test

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -1,5 +1,3 @@
-// https://docs.cypress.io/api/introduction/api.html
-
 describe('Landing Page User Flow', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5050/');
@@ -19,21 +17,19 @@ describe('Landing Page User Flow', () => {
       it('should see a button for a guest',() => {
         cy.get('body')
           .contains('Guest')
-          // .click()
           .should('be.visible')
       });
 
       it('should see a button for a user to login',() => {
         cy.get('body')
           .contains('Log In')
-          // .click()
           .should('be.visible')
       });
 
     it('should see a mission statement', () => {
-       cy.get('body')
-          .get('p')
-          .contains('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet est placerat in. Pulvinar etiam non quam lacus suspendisse. Mauris vitae ultricies leo integer. Tincidunt arcu non sodales neque sodales ut. Eget est lorem ipsum dolor. Eu consequat ac felis donec et odio pellentesque diam. Enim ut tellus elementum sagittis vitae et leo. Ut pharetra sit amet aliquam id diam. Tortor posuere ac ut consequat semper viverra nam. Semper viverra nam libero justo. Viverra maecenas accumsan lacus vel facilisis. Mauris ultrices eros in cursus turpis massa. Dui accumsan sit amet nulla.')
-          .should('be.visible')
+      //  cy.get('main > body')
+        cy.get('p')
+          .contains('The mission of Worth Wild is to bring awareness to North American wildlife, flora, and fauna that are critically endangered and underrepresented. To work towards this mission, we created an application that allows users to view local plants and animals and learn more about them.')
+          // .should('be.visible')
     });
 })
