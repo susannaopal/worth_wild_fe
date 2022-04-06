8a5f958ac53e25ccff0e688ab9dee1debdd5253d
Susanna Carey <susannaopal@gmail.com>
2022-04-06 17:54:47 -0400

    Write tests for index page with fixtures in place

--- cypress/integration/search_page_spec.js
+++ cypress/integration/search_page_spec.js
@@ -17,7 +17,53 @@ describe('Search Page User Flow', () => {
   });
 
   it('should see an animal or vegetation of the day', () => {
-    cy.get('feature-info-div')
-  })
+    cy.get('body')
+      .get('.feature-info-div')
+      .get('h3')
+      .contains('Animal of the Day')
+      .should('be.visible')
+  });
+  
+    it('should see a search bar', () => {
+      cy.get('body')
+        .get('.search-bar')
+        .should('be.visible')
+    });
 
+  it('should see the featured organization', () => {
+     cy.get('body')
+      .get('.feature-info-div')
+      .get('h3')
+      .contains('Featured Organization')
+      .get('.wwf-logo')
+      .should('be.visible')
+  });
+
+  it('should see a title for the Common Name', () => {
+    cy.get('body')
+      .get('.labels-container')
+      .contains('Common Name')
+      .should('be.visible')
+  });
+
+   it('should see a title for the Scientific Name', () => {
+    cy.get('body')
+      .get('.labels-container')
+      .contains('Scientific Name')
+      .should('be.visible')
+  });
+
+   it('should see a title for the Conservation Name', () => {
+    cy.get('body')
+      .get('.labels-container')
+      .contains('Conservation Status')
+      .should('be.visible')
+  });
+
+  it('should show three types of species shown', () => {
+      cy.get('.animal-card-link')
+        .should('have.length', 3)
+        .contains('Wyoming Toad')
+        .should('be.visible')
+  });
 });
