c90e8940d00bbbb3d1513be4ef31a3df14803112
Susanna Carey <susannaopal@gmail.com>
2022-04-05 20:10:39 -0400

    Fix broken tests for landing page

--- cypress/integration/landing_page_spec.js
+++ cypress/integration/landing_page_spec.js
@@ -27,9 +27,9 @@ describe('Landing Page User Flow', () => {
       });
 
     it('should see a mission statement', () => {
-      //  cy.get('main > body')
+       cy.get('main > body')
         cy.get('p')
           .contains('The mission of Worth Wild is to bring awareness to North American wildlife, flora, and fauna that are critically endangered and underrepresented. To work towards this mission, we created an application that allows users to view local plants and animals and learn more about them.')
-          // .should('be.visible')
+          .should('be.visible')
     });
 })
