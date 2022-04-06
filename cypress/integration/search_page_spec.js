e86ba0f6b959e90e0e27fb5803f2c561c1de8cf5
Susanna Carey <susannaopal@gmail.com>
2022-04-06 17:11:30 -0400

    Create testing fixture for search page view

---
+++ cypress/integration/search_page_spec.js
@@ -0,0 +1,23 @@
+describe('Search Page User Flow', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://secure-island-06435.herokuapp.com/api/v1/animals', {fixture: "animal_objects.json"})
+      .visit('http://localhost:5050/main');
+  });
+
+   it('should see a tagline', () => {
+    cy.get('.logo-tagline-div')
+      .contains('Are they worth it to you?')
+      .should('be.visible');
+  });
+
+  it('should see a login button on the nav', () => {
+    cy.get('body')
+      .contains('Login')
+      .should('be.visible')
+  });
+
+  it('should see an animal or vegetation of the day', () => {
+    
+  })
+
+});
