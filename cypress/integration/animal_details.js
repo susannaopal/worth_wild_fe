cd64d5e73543462426313c3637956da21d25d683
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-04-06 18:31:11 -0600

    Add additional tests for show view.

--- cypress/integration/animal_details.js
+++ cypress/integration/animal_details.js
@@ -1,5 +1,3 @@
-const cypress = require("cypress");
-
 describe('User Dashboard User Flow', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://secure-island-06435.herokuapp.com/api/v1/animals', {fixture: "animal_objects.json"})
@@ -7,9 +5,6 @@ describe('User Dashboard User Flow', () => {
       cy.get('.card-div')
       .contains('Wyoming Toad')
       .click()
-
-    // cy.intercept('GET', 'https://secure-island-06435.herokuapp.com/api/v1/animal?common_name=Wyoming Toad&element_code=AAABB01220', {fixture: "animal_details.json"})
-    //   .visit('http://localhost:5050/details');
   });
 
   it('should be able to visit the url', () => {
@@ -25,28 +20,61 @@ describe('User Dashboard User Flow', () => {
   it('should have a nav bar', () => {
     cy.get('.animal-details')
       .contains('WYOMING TOAD')
-  });
-
-  it('should show three types of species shown', () => {
-    cy.get('.scientific-class-div')
-      .should('have.length', 7)
-      // .contains('Wyoming Toad')
       .should('be.visible')
   });
-  // it('should have a phylum', () => {
-  //   cy.get('.scientific-classification')
-  //   .contains('Phylum: Craniata')
-  // });
 
+  it('should have a phylum', () => {
+    cy.get('.scientific-class-div')
+      .contains('Phylum: Craniata')
+      .should('be.visible')
+  });
+
+  it('should have a class', () => {
+    cy.get('.scientific-class-div')
+      .contains('Class: Amphibia')
+      .should('be.visible')
+  });
+
+  it('should have a order', () => {
+    cy.get('.scientific-class-div')
+      .contains('Order: Anura')
+      .should('be.visible')
+  });
+  
+  it('should have a family', () => {
+    cy.get('.scientific-class-div')
+      .contains('Family: Bufonidae')
+      .should('be.visible')
+  });
+
+  it('should have a genus', () => {
+    cy.get('.scientific-class-div')
+      .contains('Genus: Anaxyrus')
+      .should('be.visible')
+  });
+
+  it('should have a species', () => {
+    cy.get('.scientific-class-div')
+      .contains('Species: Anaxyrus baxteri')
+      .should('be.visible')
+  });
+
+  it('should have a population', () => {
+    cy.get('.threats-info')
+      .contains('Currently there are 250 - 2500 individuals in America. Wild population fluctuates and is tenuous with "very little wild breeding to date" (USFWS 2015).')
+      .should('be.visible')
+  });
+
+  it('should have a threats information', () => {
+    cy.get('.threats-info')
+      .contains('Historically associated with floodplain ponds along the Big and Little Laramie Rivers; use of lakes may have been limited due to saline conditions; irrigation may have flushed out the lakes and made them more suitable for toads (George Baxter). Currently occurs in the vicinity of lakes and adjacent meadows. Uses rodent burrows for shelter. Eggs and larvae develop in shallow water. Mortenson Lake site is infected with the amphibian chytrid fungus (Batrachochytrium dendrobatidis) (USFWS 2002). This fungus has been implicated in declines and extinctions of amphibian species worldwide. Retrospective analysis shows that the fungus has been present at Mortenson Lake since at least 1989. In addition, chytridiomycosis is the most commonly seen disease in the captive population. Predation, pesticide use, irrigation practices, and lack of genetic diversity may also limit the abundance of Wyoming toads in the Laramie Basin (USFWS 2015). Mortenson Lake has become more saline (and less suitable for toads) as a result of drought-related increases in evaporation (USFWS 2002).')
+      .should('be.visible')
+  });
+
+  it('should have a back button', () => {
+    cy.get('.details-btns')
+      .contains('Back')
+      .should('be.visible')
+  });
 });
 
-
-// describe('Pokedex User Flow', () => {
-//   beforeEach(() => {
-//     cy.intercept('GET', ' https://pokeapi.co/api/v2/pokemon-species/1/', {fixture: "pokeDetails.json"})
-//       .visit('http://localhost:3000/generation-i/0');   
-//      cy.get('[href="/generation-i"]')
-//       .click()
-//       .get('[href="/generation-i/0"] > .card-sprite')
-//       .click()
-//   });
